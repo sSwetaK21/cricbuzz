963ba901c9b380dbb225e0975d9ba4d000ff12d7
shweta <shwetakodamg@gmail.com>
2022-11-10 23:20:06 +0530

    comit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/HeaderComponents/Navbar'
+import Sidebar from './components/HeaderComponents/Sidebar'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Navbar /> */}
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
