de24c939a120175ecd9b88ef0ab36993c91a6017
Chandan2485 <chandan96kar@gmail.com>
2022-11-27 15:06:20 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // import logo from './logo.svg';
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
-// import Navbar from "./components/HeaderComponents/Navbar";
-// import Sidebar from "./components/HeaderComponents/Sidebar";
+// import "./App.css";
+import Navbar from "./components/HeaderComponents/Navbar";
+import Sidebar from "./components/HeaderComponents/Sidebar";
 // import Matches from "./components/BodyComponents/Matches/Matches";
 import News from "./components/BodyComponents/Matches/News";
 import NewsDeatail from "./components/BodyComponents/Matches/NewsDeatail";
@@ -13,21 +13,26 @@ import { Animation } from "./components/Component/Animation";
 
 import Stats from './components/BodyComponents/Stats';
 // import './App.css';
-function App() {
-  const [Id, setId] = useState([]);
+// function App() {
+//   const [Id, setId] = useState([]);
 
-  return(
+  // return(
+// import './App.css';
+function App() {
+  // const [Id, setId] = useState([]);
+  return (
     <div className="App">
+      {/* hjgjhkgk */}
       {/* <Navbar /> */}
-      {/* <Sidebar /> */}
+      <Sidebar />
       {/* <Matches /> */}
-      <Animation/>
-      <ABC.Provider value={{ Id, setId }}>
+      {/* <Animation/> */}
+      {/* <ABC.Provider value={{ Id, setId }}>
       <Routes>
         <Route path="/" element={<News />} />
         <Route path="/NewsDetail" element={<NewsDeatail />} />
       </Routes>
-      </ABC.Provider>
+      </ABC.Provider> */}
 
       <Sidebar />
       {/* <Series /> */}
